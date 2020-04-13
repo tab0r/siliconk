48e1d2637d4f28c0dc1c0ae64d6f26e77f42f519
Tabor Henderson <tabor.henderson@sling.com>
2020-04-12 22:10:07 -0600

    f

--- silichonk.py
+++ silichonk.py
@@ -3,17 +3,27 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.colors import ListedColormap
 
 # setting up the values for the grid
 EMPTY = 0
-S = 255 # "silicon" resource cell
-D = 127 # "diatom" living cell
+S = 1 # "silicon" resource cell
+D = 2 # "diatom" living cell
+# note: for color clarity this map have exactly as many entries as we have
+# cell "types"
+cmap = ListedColormap(["darkblue", "maroon", "lawngreen"])
+# "lightseagreen"
+
 vals = [EMPTY, S, D]
 
 def randomGrid(N):
 
 	"""returns a grid of NxN random values"""
-	return np.random.choice(vals, N*N, p=[0.7, 0.2, 0.1]).reshape(N, N)
+	scarce = [0.9, 0.05, 0.05]
+	slowgrowth = [0.89, 0.00, 0.11]
+	fastgrowth = [0.85, 0, 0.15]
+	return np.random.choice(vals, N*N, p=fastgrowth).reshape(N, N)
+
 
 def update(frameNum, img, grid, N):
 
@@ -40,18 +50,18 @@ def update(frameNum, img, grid, N):
 			# 		newGrid[i, j] = ON
 
 			neighbors = [grid[i, (j-1)%N], grid[i, (j+1)%N], grid[(i-1)%N, j], grid[(i+1)%N, j]]
-			totalS = sum([1 for n in neighbors if n == 255])
-			totalD = sum([1 for n in neighbors if n == 127])
+			totalS = sum([1 for n in neighbors if n == S])
+			totalD = sum([1 for n in neighbors if n == D])
 			if grid[i, j] == EMPTY:
-				if totalS > 0 and totalD > 0:
+				if totalD > 1:
 					newGrid[i, j] = D # empty cell comes alive
 			elif grid[i, j] == S:
-				if totalD > 1:
-					newGrid[i, j] = D # resource cell comes alive
-				elif totalD > 0:
+				if totalD > 2:
 					newGrid[i, j] = EMPTY # resource cell is consumed by neighbors
+				elif totalD > 1:
+					newGrid[i, j] = D # resource cell comes alive
 			elif grid[i, j] == D:
-				if totalS == 0 or totalD > 1:
+				if totalS == 0 or totalD > 2:
 					newGrid[i, j] = S # living cell dies
 
 	# update data
@@ -65,7 +75,7 @@ def main():
 	# Command line args are in sys.argv[1], sys.argv[2] ..
 	# sys.argv[0] is the script name itself and can be ignored
 	# parse arguments
-	parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+	parser = argparse.ArgumentParser(description="Runs Tabor's Food Web simulation.")
 
 	# add arguments
 	parser.add_argument('--grid-size', dest='N', required=False)
@@ -79,7 +89,7 @@ def main():
 		N = int(args.N)
 
 	# set animation update interval
-	updateInterval = 50
+	updateInterval = 1
 	if args.interval:
 		updateInterval = int(args.interval)
 
@@ -89,7 +99,7 @@ def main():
 
 	# set up animation
 	fig, ax = plt.subplots()
-	img = ax.imshow(grid, interpolation='nearest')
+	img = ax.imshow(grid, cmap, interpolation='nearest')
 	ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
 								frames = 10,
 								interval=updateInterval,
